ad30b388dd9fb13b52abf645b9cfccb7d122fa87
1-Archit-1 <architsengupta1999@gmail.com>
2026-07-08 17:07:07 -0400

    Improvements

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-change-in-production')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = ['1archit1.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
